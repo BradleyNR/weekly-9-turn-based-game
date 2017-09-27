48977cfbeab842a5c1ce84b3234867f8928664e4
Bradley_Carson <bmcarson1671@gmail.com>
2017-09-27 16:46:33 -0400

    added lose state

--- app/scripts/controllers/loadBattle.js
+++ app/scripts/controllers/loadBattle.js
@@ -30,23 +30,30 @@ function loadBattle(store){
   // ------ keep event handler on the element after an HTML update ------
   $( "#app" ).on( "click", ".js-hero-attack", function(e) {
     e.preventDefault();
-    ctx.enemyAttack = '';
-    console.log('before hp: ', state.chosenEnemy.health);
-    state.chosenHero.useAttack(state.chosenEnemy);
-    console.log('after hp: ', state.chosenEnemy.health);
 
-    ctx.attack = 'You attacked for ' + state.chosenHero.attackPower + ' damage!';
+    if (state.chosenHero.health > 0) {
+      ctx.enemyAttack = '';
+      console.log('before hp: ', state.chosenEnemy.health);
+      state.chosenHero.useAttack(state.chosenEnemy);
+      console.log('after hp: ', state.chosenEnemy.health);
+
+      ctx.attack = 'You attacked for ' + state.chosenHero.attackPower + ' damage!';
+    }
 
     if (state.chosenEnemy.health <= 0) {
       state.chosenEnemy.health = 0;
       ctx.attack = 'You Win!'
     }
 
+
     console.log(state.winScreen);
 
     $('#app').html(battleTemplate(ctx));
 
     setTimeout(function(){
+      if (state.chosenHero.health <= 0) {
+        return
+      }
       if (state.chosenEnemy.health > 0) {
         ctx.attack = ''
         ctx.enemyAttack = 'Enemy attacked for ' + state.chosenEnemy.attackPower + ' damage!'
@@ -54,6 +61,12 @@ function loadBattle(store){
         state.chosenEnemy.useAttack(state.chosenHero);
         console.log('Hero HP after: ', state.chosenHero.health);
       }
+
+      if (state.chosenHero.health <= 0) {
+        state.chosenHero.health = 0;
+        ctx.attack = 'You Lose!'
+      }
+
       $('#app').html(battleTemplate(ctx));
     }, 1000);
   });
